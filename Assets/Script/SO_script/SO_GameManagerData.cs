75b45564cc8a795f41ee36c048edaaad098cdf4a
DyzilestarHorches <ngbahoch@gmail.com>
2021-11-13 12:20:57 +0700

    add GameManagerData (SO)

---
+++ Assets/Script/SO_script/SO_GameManagerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SO_GameManagerData : ScriptableObject
+{
+    SO_MyState gameState;
+
+    Vector2 gameMove;
+    float gameSpeed;
+
+    int score;
+}
