986df82865b71bbcc6e0abdb5df9c060d6894764
DyzilestarHorches <ngbahoch@gmail.com>
2021-11-23 10:52:54 +0700

    Revert "Merge remote-tracking branch 'origin/Hoang20/11'"
    This reverts commit 2052d646130e1cc49dd752cfdb89f5ff1ea4fa40, reversing
    changes made to cfa7db4593bd0b58f8f7888902d8f80fc06896e2.

--- Assets/Script/SO_script/SO_PlatformData.cs
+++ Assets/Script/SO_script/SO_PlatformData.cs
@@ -7,9 +7,4 @@ public class SO_PlatformData : ScriptableObject
 {
     public Vector2 size;
     public Sprite image;
-    public float spawn;
-    public float despawn;
-    public float leftLimit;
-    public float rightLimit;
-    public float moveSpeed;
 }
