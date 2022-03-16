2601895cacc5f3d9f742a40d7a7dce6d2f3d7e02
DyzilestarHorches <ngbahoch@gmail.com>
2022-03-16 11:38:17 +0700

    merge done

--- Assets/Script/SO_script/SO_PlatformData.cs
+++ Assets/Script/SO_script/SO_PlatformData.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new PlatformData", menuName = "PlatformData", order = 54)]
 public class SO_PlatformData : ScriptableObject
 {
-    public Vector2 size;
+    //Scoring Rectangle
+    public Rectangle pointZone;
+    public Vector2 size, position;
     public Sprite image;
-    public float spawn;
-    public float despawn;
-    public float leftLimit;
-    public float rightLimit;
-    public float moveSpeed;
+    public float spawn = 80;
+    public float despawn = -60;
+    public float leftLimit = -50;
+    public float rightLimit = 50;
+    public float moveSpeed = 5;
 }
+
